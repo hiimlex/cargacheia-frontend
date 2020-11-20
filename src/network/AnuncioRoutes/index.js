1f29d2d33f6641780dd2c7c73adb438af329835e
Saulo Joab <saulojoabps@gmail.com>
2020-11-20 15:43:29 -0300

    listing pedidos

--- src/network/AnuncioRoutes/index.js
+++ src/network/AnuncioRoutes/index.js
@@ -7,5 +7,13 @@ export default {
         const dataJson = await data.json();
 
         return dataJson;
+    },
+
+    async getPedidos (id) {
+        const data = await fetch(`${api}/pedidos/anuncio/${id}`);
+
+        const dataJson = await data.json();
+
+        return dataJson;
     }
 }
--- src/pages/Pedidos/index.js
+++ src/pages/Pedidos/index.js
@@ -1,12 +1,32 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./styles.css";
-import { Navbar, Card } from "../../components"
+import { Navbar, Card } from "../../components";
+import { useParams } from 'react-router-dom';
+
+import Loader from 'react-loader-spinner';
+
+import { AnuncioRoutes } from './../../network';
 
 import logoImg from "../../assets/logo.svg";
 
 // Fazer as requisições para pegar os dados e jogar dentro do card com useState e etc
 
 export default function Pedidos() {
+	let { id } = useParams();
+	const [loading, setLoading] = useState(true);
+	const [pedidos, setPedidos] = useState([]);
+
+	async function getPedidos() {
+		setLoading(true);
+		const data = await AnuncioRoutes.getPedidos(id);
+
+		setPedidos(data);
+		setLoading(false);
+	}
+
+	useEffect(() => {
+		getPedidos();
+	}, [])
 
 	return (
 		<div className="dashboard-container">
@@ -14,24 +34,27 @@ export default function Pedidos() {
 			<div className="dashboard-content">
 				{/* Fazer Scrollview com os cards mantendo a imagem do lado parada no web, no mobile a imagem vai ser retirada */}
 				<div className="cards">
-					<Card 
-                    companyName="Teste"
-                    departure="Fortaleza"
-                    destination="Juazeiro do Norte"
-                    distance={3000}
-                    weight={2000}
-                    price={2000}
-					/>
-
-                    <Card 
-                    companyName="Teste"
-                    departure="Fortaleza"
-                    destination="Juazeiro do Norte"
-                    distance={3000}
-                    weight={2000}
-                    price={2000}
-					/>
-				</div>
+				{loading ? (
+						<Loader
+						type="Puff"
+						color="#00BFFF"
+						height={100}
+						width={100}
+						timeout={3000} //3 secs
+					 />
+					) : (pedidos.length === 0 ? <span>Não há pedidos ainda.</span> : pedidos.map((pedido) => {
+						return (
+							<Card 
+							companyName={pedido.empresa}
+							departure={pedido.partida}
+							destination={pedido.destination}
+							distance={3000}
+							weight={pedido.peso}
+							price={23}
+							/>
+						)
+					}))}
+					</div>
 				<div>
 					<img
 						src={logoImg}
